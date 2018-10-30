6dd7079c5e581128ca9ae2baabc2470ee1a3bb91
James Lee <james.k.lee4@gmail.com>
2018-10-29 19:02:42 -0700

    Initialize ReviewsListEntry component

---
+++ src/components/ReviewsListEntry.jsx
@@ -0,0 +1,12 @@
+gimport React from 'react';
+import styled from 'styled-components';
+import ReactStars from 'react-stars'
+
+let ReviewsListEntry = (props) => {
+
+  return (
+    <ListEntry></ListEntry>
+  );
+};
+
+export default ReviewsListEntry;

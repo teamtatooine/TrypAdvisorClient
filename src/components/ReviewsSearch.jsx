e623285a5b2bf902e84652258785fd2a0810d6bc
James Lee <james.k.lee4@gmail.com>
2018-10-29 18:49:01 -0700

    Initialize ReviewsSearch component

---
+++ src/components/ReviewsSearch.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Search = styled.div`
+`;
+
+let ReviewsSearch = (props) => {
+
+  return (
+    <Search></Search>
+  )
+};
+
+export default ReviewsSearch;

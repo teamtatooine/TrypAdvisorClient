d90e6d88e3a87f5d26c48b4bcd7639639f16c3e8
James Lee <james.k.lee4@gmail.com>
2018-10-29 18:34:10 -0700

    Initialize ReviewsService component

---
+++ src/components/ReviewsService.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import $ from 'jquery';
+import styled from 'styled-components';
+
+class ReviewsService extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return ();
+  }
+}
+
+export default ReviewsService;

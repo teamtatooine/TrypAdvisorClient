1ec1a938a7b034bd5f510588d9e414d1ea080c57
James Lee <james.k.lee4@gmail.com>
2018-10-29 18:36:33 -0700

    Render reviews header + styles

--- src/components/ReviewsService.jsx
+++ src/components/ReviewsService.jsx
@@ -2,6 +2,76 @@ import React from 'react';
 import $ from 'jquery';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  background-color: #fff;
+  padding: 16px 24px 24px 24px;
+  margin-right: 200px;
+  border-radius: 2px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #e5e5e5;
+  font-family: Arial, Tahoma, "Bitstream Vera Sans", sans-serif;
+`;
+
+const Header = styled.div`
+  padding: 0px 0px 12px;
+  display: flex;
+  font-weight: bold;
+  border-width: 0 0 1px;
+  border-style: solid;
+  border-color: #e5e5e5;
+  color: #000a12;
+`;
+
+const Title = styled.div`
+  flex-grow: 1;
+  margin-right: 5px;
+  font-size: 28px;
+  line-height: 32px;
+`;
+
+const ReviewCount = styled.span`
+  color: #767676;
+  padding-left: 12px;
+  font-size: 28px;
+  font-weight: bold;
+  line-height: 32px;
+`;
+
+const Button = styled.button`
+  -webkit-appearance: none;
+  position: relative;
+  display: inline-block;
+  padding: 8px 16px;
+  font-weight: bold;
+  font-family: inherit;
+  border: 1px solid transparent;
+  border-radius: 2px;
+  background-clip: padding-box;
+  box-sizing: border-box;
+  font-size: 14px;
+  line-height: 18px;
+  text-align: center;
+  text-decoration: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  cursor: pointer;
+  &:hover {
+    border-color: #00c094 #267060 #267060 #00c094;
+    background-color: #00c094;
+  };
+`;
+
+const ReviewButton = styled(Button)`
+  float: right;
+  border-color: #00a680 #267060 #267060 #00a680;
+  background-color: #00a680;
+  color: #fff;
+`;
+
+
+
+
 class ReviewsService extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +81,23 @@ class ReviewsService extends React.Component {
   componentDidMount() {}
 
   render() {
-    return ();
+    return (
+      <div>
+        <Container>
+
+          <Header>
+            <Title>
+              Reviews
+              <ReviewCount> 100 </ReviewCount>
+            </Title>
+            <ReviewButton as="a" href="/">
+              Write a Review
+            </ReviewButton>
+          </Header>
+
+        </Container>
+      </div>
+    );
   }
 }
 

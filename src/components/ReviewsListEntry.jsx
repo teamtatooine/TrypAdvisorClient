12834e5462e342d0ed97c9b27ee7ff0df6b15869
James Lee <james.k.lee4@gmail.com>
2018-11-06 02:23:01 -0800

    Update ReviewsListEntry component to show time passed since reviews were posted

--- src/components/ReviewsListEntry.jsx
+++ src/components/ReviewsListEntry.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import ReactStars from 'react-stars'
+import moment from 'moment';
 
 const ListEntry = styled.div`
   margin: 8px 0 4px;
   border-top: 1px solid #e5e5e5;
-  // position: relative;
-  // overflow: visible;
 `;
 
 const ReviewColumn1 = styled.div`
@@ -81,6 +80,7 @@ const ReviewDescription = styled.p`
   color: #4a4a4a;
   padding: 0;
   margin: 5px 0 0;
+  word-break: break-all;
 `;
 
 const ReviewPics = styled.div`
@@ -129,13 +129,8 @@ const UpVote = styled.div`
   width: 20%;
   overflow: hidden;
   margin-top: 12px;
-// height: 20px;
-    // padding: 0 6px;
     border: 1px solid #e5e5e5;
     border-radius: 4px;
-    // display: inline-block;
-    // background: #fff;
-    // color: #4a4a4a;
     text-align: center;
     box-sizing: border-box;
   color: #4a4a4a;
@@ -155,7 +150,7 @@ const Disclaimer = styled.div`
 
 
 
-let ReviewsListEntry = (props) => {
+const ReviewsListEntry = (props) => {
 
   return (
     <ListEntry>
@@ -176,7 +171,7 @@ let ReviewsListEntry = (props) => {
           <Rating>
             <ReactStars count={5} value={props.review.userRating} size={22} color2={'#00a680'} edit={false} />
           </Rating>
-          <RatingDate> Reviewed yesterday </RatingDate>
+          <RatingDate> Reviewed {moment(props.review.reviewDate, "YYYY-MM-DDTHH:mm:ss.sssZ").fromNow()} </RatingDate>
         </RatingContainer>
 
         <ReviewTitle> {props.review.title} </ReviewTitle>
@@ -185,8 +180,8 @@ let ReviewsListEntry = (props) => {
 
         <ReviewPics>
           <ReviewPicsWrapper>
-            {props.review.photos.map(() =>
-              <ReviewPicContainer>
+            {props.review.photos.map((photo) =>
+              <ReviewPicContainer key={photo}>
                 <ReviewImg src={"https://picsum.photos/200/300?image=" + Math.floor(Math.random() * 1000)} />
               </ReviewPicContainer>
             )}
